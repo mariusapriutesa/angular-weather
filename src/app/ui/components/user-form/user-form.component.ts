dadadc501e9b3c200928aaf28c4739f380f3cfae
mariusapriutesa <mariusapriutesa@gmail.com>
2022-04-20 13:57:24 +0200

    si

--- src/app/ui/components/user-form/user-form.component.ts
+++ src/app/ui/components/user-form/user-form.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { IUser } from 'src/app/core/domain/types';
 import { UsersService } from 'src/app/core/services/users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-user-form',
@@ -15,11 +16,11 @@ export class UserFormComponent implements OnInit {
   user:IUser| undefined;
 
   constructor(private service: UsersService , private route: ActivatedRoute) {
-
+    const emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
     this.userForm = new FormGroup({
       'id': new FormControl(this.user?.id),
       'name': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'email': new FormControl(null, [Validators.required, Validators.pattern(emailPattern)]),
       'password': new FormControl(null),
       'enabled': new FormControl(true),
       'createdAt': new FormControl(null, Validators.required)
@@ -47,9 +48,39 @@ export class UserFormComponent implements OnInit {
     const userId = this.route.snapshot.params['id'];
     if (userId && this.user?.id != undefined) {
       userData.id = this.user?.id;
-      this.service.updateUser(userData).subscribe(console.log);
+      Swal.fire({
+        title: 'Quieres actualizar el usuario?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Actualizar',
+        denyButtonText: `No actualizar`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.service.updateUser(userData).subscribe(console.log);
+          Swal.fire('El usuario ha sido guardado!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('Los cambios no han sido guardados', '', 'info')
+        }
+      })
+      
     } else {
-      this.service.addUser(userData).subscribe(console.log);
+      Swal.fire({
+        title: 'Quieres crear el nuevo usuario?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'SI',
+        denyButtonText: `No`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.service.addUser(userData).subscribe(console.log);
+          Swal.fire('El usuario ha sido guardado!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('Los cambios no han sido guardados', '', 'info')
+        }
+      })
+      
     }
   }
   }
